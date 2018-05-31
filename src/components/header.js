d93fab75dee40319bd610d53aa3a68886cf13d31
americanman <snowboardracer7@gmail.com>
2018-05-31 16:07:31 -0400

    Make Header component reusable

--- src/components/header.js
+++ src/components/header.js
@@ -3,34 +3,34 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 // Make a component
-const Header = () => {
+const Header = (props) => {
   const { textStyle, viewStyle } = styles;
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>Albums!</Text>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
 
-const styles = {
-    viewStyle: {
-        backgroundColor: '#595959',
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingTop: 20,
-        height: 60,
-      width: 350,
-      shadowColor: '#585858',
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.2,
-      elevation: 2,
-      position: 'relative'
-    },
-    textStyle: {
-        fontSize: 20,
-        color: '#fff',
-    }
-};
+  const styles = {
+      viewStyle: {
+          backgroundColor: '#595959',
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingTop: 20,
+          height: 60,
+          width: 350,
+          shadowColor: '#585858',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.2,
+          elevation: 2,
+          position: 'relative'
+      },
+      textStyle: {
+          fontSize: 20,
+          color: '#fff',
+      }
+  };
 
 // Make a componenet available for other parts of the app
 export default Header;
