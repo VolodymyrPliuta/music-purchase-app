a2883de882fb55efed2e2d3f2a11ecc034e89212
americanman <snowboardracer7@gmail.com>
2018-05-31 14:34:27 -0400

    Style header component

--- App.js
+++ App.js
@@ -17,6 +17,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#000',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start'
   },
 });
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,35 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 // Make a component
 const Header = () => {
-  return <Text style={{color: 'white'}}>Albums!</Text>;
+  const { textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
+};
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#595959',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 20,
+        height: 60,
+      width: 350,
+      shadowColor: '#585858',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.2,
+      elevation: 2,
+      position: 'relative'
+    },
+    textStyle: {
+        fontSize: 20,
+        color: '#fff',
+    }
 };
 
 // Make a componenet available for other parts of the app
