5b0ce9cf7c0d21833bbb1efb0faf349896835554
americanman <snowboardracer7@gmail.com>
2018-05-30 22:01:23 -0400

    Init commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Yoooooo! Mike dude! We are in teh game!</Text>
+        <Text>Yo! Mike dude! We are in the game!</Text>
+        <Text>We are in the game</Text>
+        <Text style={{color: 'white'}}>Each game. We are in the game</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ index.js
@@ -0,0 +1,13 @@
+// Import libraries to help create a component
+import React form 'react';
+import ReactNative form 'react-native`;
+
+// Create a componenet 
+const App = () => {
+    return (
+        <Text>Some index.js text</Text> 
+    );
+};
+
+// Render it to the device
+ReactNative.AppRegistry.registerComponent('AwesomeProject', () => App);
